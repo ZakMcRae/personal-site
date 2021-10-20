de4f5d04df615709d8b9bdc81ed2ccd48512d6b6
Zak McRae <zakmcrae@gmail.com>
2021-10-20 16:08:58 -0400

    Wrote rough draft for about me

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,29 @@ import React from "react";
 import { ContentDiv } from "./styles/Div.styled";
 
 function About() {
-  return <ContentDiv>About me</ContentDiv>;
+  return (
+    <ContentDiv>
+      <h1>Hey, my name is Zak</h1>
+      <p>
+        In late 2020 I decided to take a new career path and for all of 2021
+        committed my full time to learning web development. In the past I used
+        python for small hobby/fun projects and eventually brought that over
+        more into my previous career. Realizing how much I loved learning and
+        implementing new technologies I decided to pursue it full time.
+      </p>
+
+      <p>Life Long Learning - Never to late to learn a new tool or trick</p>
+
+      <p>
+        Passion for the field - Listen to programming podcasts for fun.
+        CoRecursive, Talk Python to Me, Syntax FM,...
+      </p>
+
+      <p>
+        Logic and Efficieny - I like the ideas and principles of programming.
+      </p>
+    </ContentDiv>
+  );
 }
 
 export default About;
