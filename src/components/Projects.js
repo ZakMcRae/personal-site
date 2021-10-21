c16c2fd28696ead652a2a8aa301c56168957d213
Zak McRae <zakmcrae@gmail.com>
2021-10-21 14:31:48 -0400

    implemented Projects and ProjectCard

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,53 @@
 import React from "react";
-import { ContentDiv } from "./styles/Div.styled";
+import {
+  ContentDiv,
+  ProjectCardDiv,
+  FlexDiv,
+  FlexGrowDiv,
+} from "./styles/Div.styled";
+import { projectsData } from "../content/projectsData";
+import SkillCard from "./skills/SkillCard";
 
 function Projects() {
-  return <ContentDiv>My Projects</ContentDiv>;
+  return (
+    <ContentDiv>
+      <h1>My Projects</h1>
+      <h4>Here is a sample of the projects I have made.</h4>
+
+      <FlexDiv>
+        {projectsData.map((project, index) => {
+          return <ProjectCard project={project} key={index} />;
+        })}
+      </FlexDiv>
+    </ContentDiv>
+  );
 }
+
+const ProjectCard = (props) => {
+  const { name, summary, projectImage, tech, repoUrl, siteUrl } = props.project;
+  return (
+    <ProjectCardDiv>
+      <img src={projectImage} alt="" />
+
+      <div>
+        <h2>{name}</h2>
+        <p>{summary}</p>
+
+        <FlexGrowDiv>
+          <h4>Tech Used</h4>
+          <div>
+            {tech.map((item, index) => {
+              return <SkillCard item={item} key={index}></SkillCard>;
+            })}
+          </div>
+        </FlexGrowDiv>
+        <FlexDiv>
+          <a href={repoUrl}>View GitHub Repo</a>
+          <a href={siteUrl}>Visit Site</a>
+        </FlexDiv>
+      </div>
+    </ProjectCardDiv>
+  );
+};
 
 export default Projects;
