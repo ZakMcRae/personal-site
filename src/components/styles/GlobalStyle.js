07dbc2eb7419709d25dbe19f18af2d8d6de7f9db
Zak McRae <zakmcrae@gmail.com>
2021-10-25 11:55:52 -0400

    removed box-sizing: border-box from global css
    was experimenting with css default
    I am too used to box-sizing: content-box for now

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -1,14 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-// change default box sizing and reset margin/padding to 0
 html {
-  box-sizing: border-box;
   font-family: sans-serif;
-}
-
-*, *:before, *:after {
-  box-sizing: inherit;
 }
 
 * {
