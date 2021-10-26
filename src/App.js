ed275f689df1868da6a703e246bf74916435cb43
Zak McRae <zakmcrae@gmail.com>
2021-10-26 16:29:40 -0400

    added theme provided for css variables

--- src/App.js
+++ src/App.js
@@ -6,17 +6,28 @@ import Projects from "./components/Projects";
 import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
 import { GlobalStyles } from "./components/styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+
+// todo redo colors into variables
+
+const theme = {
+  colors: {
+    background: "#eee",
+  },
+};
 
 const App = () => {
   return (
     <>
-      <GlobalStyles />
-      <Landing></Landing>
-      <About></About>
-      <Skills></Skills>
-      <Projects></Projects>
-      <Timeline></Timeline>
-      <Footer></Footer>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Landing></Landing>
+        <About></About>
+        <Skills></Skills>
+        <Projects></Projects>
+        <Timeline></Timeline>
+        <Footer></Footer>
+      </ThemeProvider>
     </>
   );
 };
