369367e48ca54c482c2b664701a348be24ed66d2
Zak McRae <zakmcrae@gmail.com>
2021-10-29 13:07:49 -0400

    various text/styling tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ function About() {
         In late 2020 I decided to take a new career path and for all of 2021
         committed my full time to learning web development. In the past I have
         used python for small hobby/fun projects and eventually brought that
-        over more into my previous career. Realizing how much I loved learning
-        and implementing new technologies I decided to pursue it full time.
+        over into my previous career. Realizing how much I loved learning and
+        implementing new technologies I decided to pursue it full time.
       </p>
 
       <div data-aos="fade-up" data-aos-duration="1000">
@@ -37,7 +37,7 @@ function About() {
             <FaPodcast />
           </div>
           <FlexGrowDiv>
-            <h4>Passion for the field</h4>
+            <h4>Passion for the Field</h4>
             <p>
               Listen to programming podcasts for fun and learning whats new.
             </p>
