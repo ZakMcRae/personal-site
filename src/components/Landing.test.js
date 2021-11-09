634aba92a1ae4815358261718e18fd5b5bf7e40c
Zak McRae <zakmcrae@gmail.com>
2021-11-09 15:08:15 -0500

    added more tests to landing component

--- src/components/Landing.test.js
+++ src/components/Landing.test.js
@@ -13,15 +13,28 @@ const MockLanding = () => {
 };
 
 describe("Test of Landing Section", () => {
-  test("Name is shown on page", async () => {
+  test("Name is shown on page", () => {
     render(<MockLanding />);
     const name = screen.getByText("Zak McRae");
     expect(name).toBeInTheDocument();
   });
 
-  test("Title is shown on page", async () => {
+  test("Title is shown on page", () => {
     render(<MockLanding />);
     const jobTitle = screen.getByText("Web Developer");
     expect(jobTitle).toBeInTheDocument();
+  });
+
+  test("4 links on component", () => {
+    render(<MockLanding />);
+    const links = screen.getAllByRole("link");
+    expect(links.length).toBe(4);
+  });
+
+  test("Resume label shown", () => {
+    render(<MockLanding />);
+    const resumeLabel = screen.getByText("Resume");
+    console.log(resumeLabel);
+    expect(resumeLabel).toBeInTheDocument();
   });
 });
