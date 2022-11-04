43f56dab1634333e37bffa745c04f26dc3c206db
Zak McRae <zakmcrae@gmail.com>
2022-11-04 12:14:53 -0400

    removed timeline from website
    don't feel it is necessary anymore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Landing from "./components/Landing";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Timeline from "./components/Timeline";
+// import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
 import { GlobalStyles } from "./components/styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
@@ -17,7 +17,7 @@ const App = () => {
         <About></About>
         <Skills></Skills>
         <Projects></Projects>
-        <Timeline></Timeline>
+        {/* <Timeline></Timeline> */}
         <Footer></Footer>
       </ThemeProvider>
     </>
